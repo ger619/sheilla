c6bc4179045ab2ffb6023ec3ac3034e00ac751c1
ger619 <draker74@gmail.com>
2023-05-17 18:59:15 +0300

    Add all

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,45 @@
+ import CountUp from "react-countup";
+
+import { useInView } from "react-intersection-observer";
+
+// import { motion } from "framer-motion";
+
+// import { fadeIn } from "../variants";
 
 const About = () => {
-  return (
-      <div className="section" id='about'>
-        About
-      </div>
+    const { ref, inView } = useInView({
+        threshold: 0.5,
+    });
+    return (
+      <section className="section" id='about' ref={ref}>
+          <div className="container mx-auto">
+              <div>
+                  <div className="flex-1 bg-about bg-contain bg-no-repeat h-[540px] mix-blend-lighten bg-top">
+                  </div>
+                  <div>
+                      <h2>About me.</h2>
+                      <h3>Bachelor of Laws degree at the Africa Nazarene University.</h3>
+                      <p>
+                          Post Graduate Diploma at the Kenya School of Law.
+                      </p>
+                      <p>
+                          Certified Professional Mediator. CPM (mti) East Africa
+                      </p>
+                      <div>
+                          <div className="flex" >
+                              <div className='text-[40px] font-tertiary text-gradient'>
+                                  {inView ? <CountUp start={0} end={5} duration={3} /> : null}
+                              </div>
+                              <div className='font-primary text-sm tracking-[2px]'>
+                                  Years of <br />
+                                  Experience
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
   );
 };
 
